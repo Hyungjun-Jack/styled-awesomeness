79e479ec1b9b583937f31cba992c7568aa25698f
Jack Kim <lovehjun@gmail.com>
2018-09-28 17:28:16 +0900

    lecture 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
-import "./App.css";
+import styled from "styled-components";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Button danger></Button>
-        <Button></Button>
-      </React.Fragment>
-    )
+      <Container>
+        <Button danger>Hello</Button>
+        <Button>Hello</Button>
+      </Container>
+    );
   }
 }
 
-const Button = ({danger}) => (
-  <button className={danger ? "button button--danger" : "button button--success"}>Hello</button>
-);
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #34495e;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 300px;
+  min-height: 300px;
+  color: white;
+  cursor: pointer;
+  font-weight: 600;
+  -webkit-appearance: none;
+  &active,
+  &focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#c0392b" : "#2ecc71")};
+`;
 
 export default App;
