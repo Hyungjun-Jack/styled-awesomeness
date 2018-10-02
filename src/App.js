9baa3002e479f8d96447a19a552fcec9b7b1fb68
Hyungjun Kim <lovehjun@gmail.com>
2018-10-02 22:14:52 +0900

    lecture 4, animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import styled, { injectGlobal } from 'styled-components';
+import React, { Component } from "react";
+import styled, { injectGlobal, keyframes } from "styled-components";
 
 injectGlobal`
   body{
@@ -9,42 +9,54 @@ injectGlobal`
 `;
 
 class App extends Component {
-	render() {
-		return (
-			<Container>
-				<Button danger>Hello</Button>
-				<Button>Hello</Button>
-				<Anchor href="https://google.com" danger>
-					Go to Google
-				</Anchor>
-			</Container>
-		);
-	}
+  render() {
+    return (
+      <Container>
+        <Button danger durationTime={2}>Hello</Button>
+        <Button>Hello</Button>
+        <Anchor href="https://google.com">Go to Google</Anchor>
+      </Container>
+    );
+  }
 }
 
 const Container = styled.div`
-	height: 100vh;
-	width: 100%;
-	background-color: #34495e;
+  height: 100vh;
+  width: 100%;
+  background-color: #34495e;
 `;
 
 const Button = styled.button`
-	border-radius: 50px;
-	padding: 5px;
-	min-width: 300px;
-	min-height: 300px;
-	color: white;
-	cursor: pointer;
-	font-weight: 600;
-	-webkit-appearance: none;
-	&active,
-	&focus {
-		outline: none;
-	}
-	background-color: ${(props) => (props.danger ? '#c0392b' : '#2ecc71')};
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 300px;
+  min-height: 300px;
+  color: white;
+  cursor: pointer;
+  font-weight: 600;
+  -webkit-appearance: none;
+  &active,
+  &focus {
+    outline: none;
+  }
+	background-color: ${props => (props.danger ? "#c0392b" : "#2ecc71")};
+	${props => {
+		if(props.danger){
+			return `animation: ${rotation} ${props.durationTime}s linear`;
+		}
+	}}
 `;
 
-const Anchor = Button.withComponent('a').extend`
+const rotation = keyframes`
+from{
+	transform:rotate(0deg)
+}
+to{
+	transform:rotate(360deg)
+}
+`
+
+const Anchor = Button.withComponent("a").extend`
   text-decoration:none;
 `;
 
